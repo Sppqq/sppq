5ea854ef83cf7bf0f91cca5e78b68313b733f76e
Sppqq <66029455+Sppqq@users.noreply.github.com>
2024-01-24 22:10:22 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+
+
+# Note: keep requirements here to ease distributions packaging
+tests_require = []
+dev_require = []
+install_requires = [
+    'pip',
+    'setuptools',
+    'g4f',
+]
+install_requires_win_only = [
+    'colorama>=0.2.4',
+]
+
+# Conditional dependencies:
+
+
+
+setup(
+    name='Sppq',
+    version='99',
+    description='None',
+    long_description='',
+    long_description_content_type='text/markdown',
+    url='https://github.com/Sppqq/sppq',
+    download_url=f'https://github.com/Sppqq/sppqs',
+    author='Sppq',
+    author_email='sppqq@duck.com',
+    license='None',
+    packages=find_packages(include=['httpie', 'httpie.*']),
+    entry_points={},
+    python_requires='>=3.7',
+    install_requires=install_requires,
+    classifiers=[],
+    project_urls={
+        'GitHub': 'https://github.com/Sppqq/sppq'
+    },
+    data_files=[]
+)
