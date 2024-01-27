fa3c4ac389237594a22c5349ee3e28690e69f63c
Sppqq <66029455+Sppqq@users.noreply.github.com>
2024-01-28 01:03:40 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,36 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-
-# Note: keep requirements here to ease distributions packaging
-tests_require = []
-dev_require = []
-install_requires = [
-    'pip',
-    'setuptools',
-    'g4f',
-]
-install_requires_win_only = []
-
-
-setup(
-    name='Sppq',
-    version='1.0',
-    description='None',
-    long_description='',
-    long_description_content_type='text/markdown',
-    url='https://github.com/Sppqq/sppq',
-    download_url=f'https://github.com/Sppqq/sppq',
-    author='Sppq',
-    author_email='sppqq@duck.com',
-    license='None',
-    packages=find_packages(include=['httpie', 'httpie.*']),
-    entry_points={},
-    python_requires='>=3.7',
-    install_requires=install_requires,
-    classifiers=[],
-    project_urls={
-        'GitHub': 'https://github.com/Sppqq/sppq'
-    },
-    data_files=[]
-)
+setup(name='sppq',
+      version='0.1',
+      description='Testing libs',
+      packages=['sppq'],
+      author_email='sppqq@duck.com',
+      zip_safe=False)
