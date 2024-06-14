0f80b215e443d6dd8b7548dc8bfc96de2bdce596
Sppqq <sppqq@duck.com>
2024-06-14 21:58:32 +0300

    Update test_sppq.py

--- tests/test_sppq.py
+++ tests/test_sppq.py
@@ -1,4 +1,3 @@
 from sppq import *
 
-printt(send_webhook(webhook_url='https://discord.com/api/webhooks/1206271216164282368/a809-Pkw6HyHhFvBq0Q0iC6y2DwsLRpMIREOJ_rMqymCbAUHR0kDJXDd9DYkNoNj9cOu',
-                    content = 'Привет', username='❖Sergey❖', avatar_url='http://tinyurl.com/2y7fcvlp'))
+printt('test complete')
